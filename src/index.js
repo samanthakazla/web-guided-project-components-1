24ebe3f3f116a398649c855fedee1480b7a07c50
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-13 12:43:41 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -104,8 +104,10 @@ function makePanel({ title, content }) {
   // don't forget to return the panel!
   return panel;
 }
-const test = makePanel({ title: "test title", content: "test content" });
-accordion.appendChild(test);
+// const test = makePanel({ title: "test title", content: "test content" });
+// accordion.appendChild(test);
+
+
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
