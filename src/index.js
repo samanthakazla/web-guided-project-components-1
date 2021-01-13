7de326365b28e5e7614f24326ed4a82e52b18ccb
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-13 11:58:22 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ function makePanel({ title, content }) {
   // don't forget to return the panel!
   return null;
 }
-makePanel({title: 'test title', });
+makePanel({ title: "test title", content: "test content" });
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
