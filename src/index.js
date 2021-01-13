d107891ff4fd6478a5e866f33c904c8a93ba1d55
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-13 11:34:35 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -2,55 +2,56 @@
 //  that takes an { imgURL } and returns an img element.
 //  Then loop over these URLs making images as you go:
 const imageData = [
-  { imageURL: 'https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_978.jpg' },
-  { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_3398.jpg' },
-  { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg' },
-]
+  {
+    imageURL: "https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_978.jpg",
+  },
+  { imageURL: "https://images.dog.ceo/breeds/mastiff-bull/n02108422_3398.jpg" },
+  { imageURL: "https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg" },
+];
 
+// the component is an image that takes raw data
 function makeImage({ imageURL }) {
-  const imgElement = document.createElement("img");
+  // we learned some new syntax: destructuring
+  const imgElement = document.createElement("img"); // make a detached img tag
   imgElement.style.width = "10em";
-  imgElement.src = imageURL;
+  imgElement.src = imageURL; // we set the image property to be the image we passed in
   return imgElement;
 }
 
+// loop where we use the component!
 imageData.forEach((imgObj) => {
   // 1 - make an image
   const img = makeImage(imgObj);
   // 2 - attach to DOM
-  document.body.prepend(img);
+  document.body.prepend(img); // attach to DOM
 });
 // TASK 1- Import the data we need to "hydrate" our component.
 //  On the one hand, the default export from data/panelData.js
 //  On the other hand, the default export from data/constants.js
 //  Destructure `open` and `close` from the constants
-
+import panelData from "./data/panelData";
+import arrows from "./data/constants";
 
 // TASK 2- Verify our imports using log statements
-console.log() // log the panelData
-console.log() // log the open arrow
-console.log() // log the close arrow
-
+console.log(panelData); // log the panelData
+console.log(arrows.open); // log the open arrow
+console.log(arrows.close); // log the close arrow
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
-
+const accordion = null;
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel(/* what data does the panel need? */) {
-
-
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
-
+  const panel = null;
+  const panelBar = null;
+  const panelContent = null;
+  const panelTitle = null;
+  const panelButtons = null;
+  const openButton = null;
+  const closeButton = null;
 
   // TASK 6- Setup the structure of our elements
   /*
@@ -66,30 +67,24 @@ function makePanel(/* what data does the panel need? */) {
     </div>
   */
 
-
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
 
-
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
   //  - the close button needs to show (the 'hide-btn' class name controls this)
   //  - the contents need to show (the 'toggle-on' class name controls this)
 
-
   // don't forget to return the panel!
-  return null
+  return null;
 }
-
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
 //  We can do this with a single forEach, or with a map and a forEach.
-
 
 // [STRETCH] Comment out the links inside the nav and
 // write a linkMaker that takes { href, className, text }
