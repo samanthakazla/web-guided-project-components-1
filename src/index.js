dba7350e91d95524f9d30e7e953ee2e8c282cc5c
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-13 12:17:26 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -77,6 +77,11 @@ function makePanel({ title, content }) {
   // paying attention to the elements that need to start out hidden
   panel.classList.add("panel");
   panelBar.classList.add("panel-bar");
+  panelButtons.classList.add("panel-buttons");
+  panelContent.classList.add("panel-content");
+  openButton.classList.add("panel-btn-open");
+  closeButton.classList.add("panel-btn-close", "hide-btn");
+  
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
 
